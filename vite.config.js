d05f5fb630dfbc6ef4ca4770593d933580b6101f
Lau-szutner <lautaroszutner@gmail.com>
2024-03-30 20:20:44 -0300

    subo repo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Praga-React/",
+  base: "https://lau-szutner.github.io/Praga-React/git",
 });
